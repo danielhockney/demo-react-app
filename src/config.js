a48d7a34049265a02530c1ee79064e6f3f3435e8
danielhockney <48559682+danielhockney@users.noreply.github.com>
2019-03-14 19:03:08 +0400

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const production = false; // set it to true when deploy to the server
+
+const domain =  '127.0.0.1:3001';
+export const websocketUrl = `ws://${domain}`
+export const apiUrl = `http://${domain}`
